08c7ac7bfe614dc3db00be22cf051e46b8234c7e
Jordan Earls <earlz@earlz.net>
2015-03-21 01:00:29 -0400

    Retarget to .NET 3.5, use a volatile field for storage of functions, add thread-safe modifications to the interceptor

--- InceptorAssembly/InceptorInterceptor.cs
+++ InceptorAssembly/InceptorInterceptor.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Earlz.InceptorAssembly
 {
@@ -14,58 +13,39 @@ namespace Earlz.InceptorAssembly
             Interceptors = new Dictionary<string, Interceptor>();
         }
         public delegate object Interceptor(object targetThis, string name, object[] parameters);
-        static Dictionary<string, Interceptor> Interceptors;
+        static volatile Dictionary<string, Interceptor> Interceptors;
         static string locked = "";
-        public static object Check(object targetThis, string name, object[] parameters) //, params object[] values)
+        public static object Check(object targetThis, string name, object[] parameters) 
         {
-            // lock(locked)
+            // store pointer here because Interceptors can be replaced at any time
+            var tmp = Interceptors;
+            if (tmp.ContainsKey(name))
             {
-                if (Interceptors.ContainsKey(name))
-                {
-                    return Interceptors[name](targetThis, name, parameters);
-                }
+                return tmp[name](targetThis, name, parameters);
             }
             return null;
         }
-        public static object[] bleh(ref int foo, params object[] p)
+        static string ModifyLock = "";
+        public static void Add(string name, Interceptor func)
         {
-            var arr=new object[2];
-            arr[0]=foo;
-            arr[1]=10;
-            foo = 20;
-            var tmp = (object[])Check(null, "foo", null);
-            if (tmp != null)
+            lock (ModifyLock)
             {
-                return tmp;
+                var d = new Dictionary<string, Interceptor>(Interceptors);
+                d.Add(name, func);
+                Interceptors = d; //assign here so it's lock-free and thread-safe.
             }
-            Console.WriteLine("meh");
-            return p;
         }
-        public static void meh()
+        public static void Remove(string name)
         {
-            int tmp = 0;
-            bleh(ref tmp, 10, 15, 50, 20);
-        }
-        public static object Coerce<T>(T anything)
-        {
-            return (object)anything;
-        }
-        public struct FooStruct
-        {
-            public int biz;
-            public int baz;
-        }
-        public static void testit(ref FooStruct test, ref string me, ref bool ugh)
-        {
-            int foo=10;
-            string blah="";
-            var store=new object[10];
-           // store[0] = test;
-            store[0]=Coerce(test);
-            //store[0]=Coerce(meh);
-            store[0]=Coerce(blah);
-            ugh = (bool)store[0];
-            test = (FooStruct)store[1];
+            lock(ModifyLock)
+            {
+                var d = new Dictionary<string, Interceptor>(Interceptors);
+                if (d.ContainsKey(name))
+                {
+                    d.Remove(name);
+                }
+                Interceptors = d; //assign here so it's lock-free and thread-safe.
+            }
         }
     }
 }
